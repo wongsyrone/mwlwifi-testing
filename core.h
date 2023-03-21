9dde869c353d2e84a8d3711e4b9d3a326f2a9789
jbsky <webmaster@jbsky.fr>
2023-03-21 15:18:49 +0100

    fix AMPDU stop flush cont case pcie_tx_del_ampdu_pkts does not clean up packets from the station. mwl_stream finally stores the queue number for purging.
    Signed-off-by: Julien Blais webmaster@jbsky.fr

--- core.h
+++ core.h
@@ -186,6 +186,8 @@ struct mwl_ampdu_stream {
 	u8 tid;
 	u8 state;
 	int idx;
+	int desc_num;
+	unsigned long jiffies;
 };
 
 struct mwl_stnid {
--- hif/pcie/tx.c
+++ hif/pcie/tx.c
@@ -1085,6 +1085,7 @@ void pcie_tx_xmit(struct ieee80211_hw *hw,
 
 
 				if (txpriority >= SYSADPT_TX_WMM_QUEUES) {
+					stream->desc_num = index;
 					spin_unlock_bh(&priv->stream_lock);
 					tx_ctrl = (struct pcie_tx_ctrl *)tx_info->driver_data;
 					tx_ctrl->sta = (void *)sta;
@@ -1152,12 +1153,18 @@ void pcie_tx_xmit(struct ieee80211_hw *hw,
 	/* Initiate the ampdu session here */
 	if (start_ba_session) {
 		spin_lock_bh(&priv->stream_lock);
-		rc = mwl_fwcmd_start_stream(hw, stream);
-		if (rc)
-			mwl_fwcmd_remove_stream(hw, stream);
-		else
-			wiphy_debug(hw->wiphy, "Mac80211 start BA %pM\n",
-				    stream->sta->addr);
+
+		if (jiffies - stream->jiffies > 5 * HZ / 1000) {
+			rc = mwl_fwcmd_start_stream(hw, stream);
+			if (rc)
+				mwl_fwcmd_remove_stream(hw, stream);
+			else
+				wiphy_debug(hw->wiphy, "Mac80211 start BA %pM\n",
+					stream->sta->addr);
+
+			stream->jiffies = jiffies;
+		}
+
 		spin_unlock_bh(&priv->stream_lock);
 	}
 }
@@ -1213,18 +1220,15 @@ void pcie_tx_del_pkts_via_sta(struct ieee80211_hw *hw,
 }
 
 void pcie_tx_del_ampdu_pkts(struct ieee80211_hw *hw,
-			    struct ieee80211_sta *sta, u8 tid)
+			    struct ieee80211_sta *sta, u8 desc_num)
 {
 	struct mwl_priv *priv = hw->priv;
 	struct pcie_priv *pcie_priv = priv->hif.priv;
-	int ac, desc_num;
 	struct sk_buff *skb, *tmp;
 	struct ieee80211_tx_info *tx_info;
 	struct pcie_tx_ctrl *tx_ctrl;
 	unsigned long flags;
 
-	ac = utils_tid_to_ac(tid);
-	desc_num = SYSADPT_TX_WMM_QUEUES - ac - 1;
 	spin_lock_irqsave(&pcie_priv->txq[desc_num].lock, flags);
 	skb_queue_walk_safe(&pcie_priv->txq[desc_num], skb, tmp) {
 		tx_info = IEEE80211_SKB_CB(skb);
